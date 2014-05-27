33f822adf415af526e6e5b06bc1906e5d62840f4
arlliu <ext-arlen.1.liu@here.com>
2014-05-27 10:24:35 +0800

    provide 3 route options

--- Code/RoutesMain.py
+++ Code/RoutesMain.py
@@ -14,12 +14,16 @@ class RoutesMain:
     Server_Address="211.151.53.78"
     Debug=False
 
+    DefaultMode='fastest;car;traffic:disabled;'
+    NotSuggestMotorWayMode='fastest;car;traffic:disabled;motorway:-2'
+    NoTollMode='fastest;car;traffic:disabled;tollroad:-3'
+    
     #GEO "latitude","longitude"
-    def listRoutes(self, geo1,geo2):
+    def listRoutes(self, geo1,geo2,mode,number):
         conn=httplib.HTTPConnection(self.Server_Address)
         headers={'accept':'application/json'}
-        alternatives= '2'
-        route_mode='fastest;car;traffic:disabled;'
+        alternatives= str(number -1)
+        route_mode=mode
         uri ="/routing/7.2/calculateroute.json?routeattributes=wp,sm,lg&maneuverattributes=ac,po,tt,le,li&linkattributes=sh&legattributes=sh&jsonAttributes=41&verboseMode=5&metricSystem=metric&alternatives="
         uri =uri + alternatives + "&waypoint0=geo!"+geo1[0]+","+geo1[1]+"&waypoint1=geo!"+geo2[0]+","+geo2[1]+"&language=zh_CN&mode="
         uri =uri + route_mode + "&app_id=90oGXsXHT8IRMSt5D79X&token=JY0BReev8ax1gIrHZZoqIg"
@@ -74,16 +78,22 @@ class RoutesMain:
     #arrived_time= yyyyddmmHHMM
     def getRoutes(self,geo0,geo1,arrived_time):
         routeOption=[]
-        routes_info=self.listRoutes(geo0,geo1)
+        routes_info=self.listRoutes(geo0,geo1,self.DefaultMode,3)
         
+        if(len(routes_info)<3):
+            routes_ext=self.listRoutes(geo0,geo1,self.NotSuggestMotorWayMode,2)
+            for route in routes_ext:
+                routes_info.append(route)
+        
+        print len(routes_info)
+      
         for route in routes_info:
             ad_route=AdRoute(route,geo0,geo1,'',arrived_time)
             ad_route.calByArrivalTime()
             routeOption.append(ad_route)
             
             print ad_route.StartTime
-        
-            
+
         return routeOption
     
 
@@ -96,6 +106,12 @@ class RoutesMain:
         start_geoY="116.41934259"
         end_geoX="39.9992530"
         end_geoY="116.4744977"
+        
+        start_geoX="39.91005"
+        start_geoY="116.65712"
+        end_geoX="39.90383"
+        end_geoY="116.39496"
+        
         planned_time="201406090900"
         
         self.getRoutes([start_geoX,start_geoY],[end_geoX,end_geoY],planned_time)
@@ -108,10 +124,10 @@ class RoutesMain:
         end_geoX="39.871680"
         end_geoY="116.439420"
         
-        start_geoX="39.9515592744"
-        start_geoY="116.41934259"
-        end_geoX="39.9992530"
-        end_geoY="116.4744977"
+        start_geoX="39.91005"
+        start_geoY="116.65712"
+        end_geoX="39.90383"
+        end_geoY="116.39496"
         
         routes=self.listRoutes([start_geoX,start_geoY],[end_geoX,end_geoY])
         
@@ -130,7 +146,7 @@ def main():
     
     #my_route.testListRoutes()
     
-    #211.151.53.78/routing/7.2/calculateroute.json?/routing/7.2/calculateroute.json?routeattributes=wp,sm,lg&maneuverattributes=ac,po,tt,le,li&linkattributes=sh&legattributes=sh&jsonAttributes=41&verboseMode=5&metricSystem=metric&alternatives=3&waypoint0=geo!39.9515592744,116.41934259&waypoint1=geo!39.9992530,116.4744977&language=zh_CN&mode=fastest;car;traffic:disabled;&app_id=90oGXsXHT8IRMSt5D79X&token=JY0BReev8ax1gIrHZZoqIg
+    #211.151.53.78/routing/7.2/calculateroute.json?/routing/7.2/calculateroute.json?routeattributes=wp,sm,lg&maneuverattributes=ac,po,tt,le,li&linkattributes=sh&legattributes=sh&jsonAttributes=41&verboseMode=5&metricSystem=metric&alternatives=3&waypoint0=geo!39.91005,116.65712&waypoint1=geo!39.90383,116.39496&language=zh_CN&mode=fastest;car;traffic:disabled;&app_id=90oGXsXHT8IRMSt5D79X&token=JY0BReev8ax1gIrHZZoqIg
 
 
 if __name__=='__main__':
